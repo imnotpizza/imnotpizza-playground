19fa13952c61c594959d2c3c253fbd1177c66561
binstat24 <binstat24@gmail.com>
2020-02-07 18:26:10 +0900

    02/07 테이블 프로젝트

--- src/App.js
+++ src/App.js
@@ -1,82 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Square from "./components/ex1.js"
-import Person from "./components/Person.js"
-
-import Button from "./components/Button"
-import Display from "./components/Display"
+import React from "react";
+import Customer from "./customer-comp/Customer";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import ProgressUI from "@material-ui/core/CircularProgress";
+import axios from "axios";
 
 /**
  * props : 하위컴포넌트로 전달하는 값, vue의 v-bind=props와 동일
  * state : 컴포넌트에서 내부적으로 처리하는 변수 저장, vue 의 data와 동일
  * props : 부모에서 사용된 태그 내의 값을 전달, vue의 slot?? 과 동일
+ *
+ * 라이프사이클
  * 
+ * constructor()
  * 
+ * componentWillMount()
  * 
+ * render() 
+ * 
+ * componentDidMount : 컴포넌트의 마운트가 완료되었을때, vue의 mounted훅과 동일
+ *
+ * shouldComponentUpdate() : props, state 변경시
  */
-// class App extends React.Component {
-  
-//   state={
-//     person:[
-//       {id: 1, name: "name1"},
-//       {id: 2, name: "name2"},
-//       {id: 3, name: "name3"},
-//       {id: 4, name: "name4"},
-//       {id: 5, name: "name5"},
-//     ]
-//   }
 
-//   render(){
-
-//     const myfun= ()=>{ "testone" };
-
-
-//     return (
-//       <div className="App">
-//       <header className="App-header">
-//         <div>
-//           <Square value={10}></Square>
-//           <Person name={"name1"} age={26} myfun={myfun}>
-//             <div>from props to children</div>
-//           </Person>
-//           <Person name={"name2"} age={36}></Person>
-//           <Person name={"name3"} age={46}></Person>
-//         </div>
-//       </header>
-//     </div>
-//     )
-//   }
-
-  
-// }
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      number: 0,
-    }
+  state = {
 
-    //this.onClickHandler=this.onClickHandler.bind(this)//react의 this를 함수내로 바인딩, 
-    //애로우 함수 사용시 this.를 react 객체로 그대로 사용하기 때문에 필요없음
+    completed: 0,
+    apidata: null,
+
+    items: [
+      {
+        id: 1,
+        image: "https://placeimg.com/64/64/1",
+        name: "name1",
+        birthday: "19921218",
+        gender: "Male",
+        job: "job1"
+      },
+      {
+        id: 2,
+        image: "https://placeimg.com/64/64/2",
+        name: "name2",
+        birthday: "11921218",
+        gender: "Male",
+        job: "job2"
+      }
+    ],
+
+
+  };
+
+  progress=()=>{
+    const { completed }=this.state;
+    this.setState({ completed: (completed>=100 ? 0 : completed+1) })
   }
-  onClickHandler = (num) => {
-    console.log("click")
-    //this.state.number++;//setState를 사용하여야 가능
-    this.setState({ number: this.state.number + num})
+
+  componentDidMount(){
+    let _timer=setInterval(this.progress, 20);
+    
+    console.log(_timer)
+    this.callApi()
+    .then(response=>{
+      this.setState({ apidata: response.data });
+    })
+  }
+  
+  callApi = async() =>{
+    const response = await axios.get("https://api.hnpwa.com/v0/news/1.json");
+
+    return response
   }
 
+  
+  
 
-  render(){
+  render() {
+    return (
+      <div>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.items.map((item, id) => (
+              <Customer userInfo={item} key={id}></Customer>
+            ))}
+          </TableBody>
+        </Table>
 
-    return(
-      <div className="Header">
-        <Display value={this.state.number}></Display>
-        <Button className="Btn" clickHandler={()=>this.onClickHandler(1)}>버튼 컴포넌트 증가</Button>
-        <Button className="Btn" clickHandler={()=>this.onClickHandler(-1)}>버튼 컴포넌트 감소</Button>
-
-        <button onClick={()=>this.onClickHandler(1)}>증가</button>
-        <button onClick={()=>this.onClickHandler(-1)}>감소</button>
+        <div>
+          {(this.state.apidata === null ?
+           "...." : 
+           this.state.apidata.map(item => <p>{item.id}</p>)
+          )}
+        </div>
+        {this.state.completed}
+        <ProgressUI></ProgressUI>
       </div>
     );
   }
