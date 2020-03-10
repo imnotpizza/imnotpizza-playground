36f27708b7cc4bf3dad0315be1c763aef8ea0cb7
binstat24 <binstat24@gmail.com>
2020-03-10 12:21:13 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,53 +1,21 @@
 import React from "react";
-import Study from "./components/Study";
-import ScrollBox from "./components/ScrollBox";
-import MovieList from "./components/MovieList";
-import SearchView from "./components/SearchView";
-import Hooks from "./components/Hooks"
-import { render } from "react-dom";
+import './index.css'
 
 
 export default class App extends React.Component {
 
-  state={
-    password: "",
-    clicked: false,
-    validatd: false,
-  }
-
-  input=React.createRef();
-
-  handleFocus=()=>{
-    this.input.current.focus();
-  }
-
-  handleChange=(e)=>{
-    this.setState({
-      password: e.target.value
-    })
-  }
-
-  handleButtonClick=()=>{
-    this.setState({
-      clicked: true,
-      validated: this.state.password==='0000'
-    })
-
-    
-  }
-
-  getVldResult=()=>{
-
-  }
+  
 
   render(){
     return(
-      <div>
-        <SearchView></SearchView>
-        <MovieList></MovieList>
-        <Hooks></Hooks>
-         
+      <div >
+        <div className="toggle"></div>
 
+        <hr></hr>
+        <h1>0</h1>
+        <button id="increase">+1</button>
+        <button id="decrease">-1</button>
+     
       </div>
     )
   }
